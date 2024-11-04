5a7e384913c7746469d2da65c2ae98b7d0408d9c
Riccardo Carta - riccardo.carta2@studio.unibo.it <riccardo.carta2@studio.unibo.it>
2024-11-04 12:08:44 +0100

    Implemented part1 requests of exercise 7.1

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,5 +1,44 @@
 package it.unibo.inner.impl;
 
-public class IterableWithPolicyImpl {
-    
+import java.util.Iterator;
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+/**
+ * IterableWithPolicy implementation.
+ */
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
+
+    private final T[] array;
+
+    public IterableWithPolicyImpl(T[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public Iterator<T> iterator() {
+        class InnerIterator implements Iterator<T>{
+            private int current;
+
+            public InnerIterator() {
+                this.current = 0;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return this.current < IterableWithPolicyImpl.this.array.length;
+            }
+            
+            @Override
+            public T next() {
+                return IterableWithPolicyImpl.this.array[current++];
+            }
+        }
+        return new InnerIterator();
+    }    
 }
